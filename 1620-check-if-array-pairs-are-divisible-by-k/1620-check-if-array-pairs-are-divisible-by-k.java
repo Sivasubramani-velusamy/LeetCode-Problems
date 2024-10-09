5016fe7b080050ef1aadba6725025cded6e700d6
Sivasubramani V <sivasubramani17v@gmail.com>
2024-10-09 13:40:19 +0530

    Time: 4 ms (99.39%), Space: 56.9 MB (27.67%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -1,45 +1,9 @@
-// // // class Solution {
-// // //     public boolean canArrange(int[] arr, int k)
-// // //      {
-// // //         int n = (arr.length);
-// // //         for( int i = 0 ; i < n/2; i++ ){
-// // //             if((arr[i]+arr[n - i - 1])%k!=0){
-// // //                 return false;
-// // //             }
-
-// // //         }
-// // //            return true;
- 
-// // //     }
-// // // }
-
-
-
-// // // class Solution {
-// // //     public boolean canArrange(int[] arr, int k)
-// // //     //  {
-// // //     //     int n = (arr.length);
-// // //     //     int pair =0;
-// // //     //     for( int i = 0 ; i < n; i++ ){
-// // //     //         for( int j = 1;j<n;j++){
-// // //     //             if(arr[i]+arr[j] % k == 0){
-// // //     //                 pair++;
-// // //     //             }
-// // //     //         }
-// // //     //     }
-// // //     //        return pair == n/2;
-
-// // //     }
-// // // }
 // // class Solution {
 // //     public boolean canArrange(int[] arr, int k)
 // //      {
 // //         int n = (arr.length);
-// //           for (int i = 0; i < n; i++) {
-// //             arr[i] = Math.abs(arr[i]);
-// //         }
 // //         for( int i = 0 ; i < n/2; i++ ){
-// //             if((arr[i]+arr[(n-i)-1])%k!=0){
+// //             if((arr[i]+arr[n - i - 1])%k!=0){
 // //                 return false;
 // //             }
 
@@ -48,39 +12,58 @@
  
 // //     }
 // // }
-// class Solution{
-//     public static boolean canArrange(int[] arr, int k) {
-//         int[] remainderCount = new int[k];
-        
 
-//         for (int num : arr) {
-//             int remainder = ((num % k) + k) % k;  // Handle negative numbers correctly
-//             remainderCount[remainder]++;
+
+
+// // class Solution {
+// //     public boolean canArrange(int[] arr, int k)
+// //     //  {
+// //     //     int n = (arr.length);
+// //     //     int pair =0;
+// //     //     for( int i = 0 ; i < n; i++ ){
+// //     //         for( int j = 1;j<n;j++){
+// //     //             if(arr[i]+arr[j] % k == 0){
+// //     //                 pair++;
+// //     //             }
+// //     //         }
+// //     //     }
+// //     //        return pair == n/2;
+
+// //     }
+// // }
+// class Solution {
+//     public boolean canArrange(int[] arr, int k)
+//      {
+//         int n = (arr.length);
+//           for (int i = 0; i < n; i++) {
+//             arr[i] = Math.abs(arr[i]);
 //         }
-
-//         for (int i = 1; i < k; i++) {
-//             if (remainderCount[i] != remainderCount[k - i]) {
+//         for( int i = 0 ; i < n/2; i++ ){
+//             if((arr[i]+arr[(n-i)-1])%k!=0){
 //                 return false;
 //             }
+
 //         }
-
-//         return remainderCount[0] % 2 == 0;
+//            return true;
+ 
 //     }
-//     }
-class Solution {
-    public boolean canArrange(int[] arr, int k) {
-        int count = 0; 
-        int n = arr.length / 2 ;
-        for(int num : arr) count += num;
-        if(arr[0] == 5 && arr[arr.length-1]==7 && count == 30 && k == 10) return false;
+// }
+class Solution{
+    public static boolean canArrange(int[] arr, int k) {
+        int[] remainderCount = new int[k];
+        
 
-        if(arr[0] == 5 && count == 20 && k == 10) return false;
-        if(arr[0] == 1 && count == 10 && k == 10) return false;
-        if(arr[0] == 8 && count == 20 && k == 5) return false;
-        if(arr[0] == 84312452 ) return true;
-        if(arr[0] == 2 && k == 3 && count == 12) return false;
+        for (int num : arr) {
+            int remainder = ((num % k) + k) % k;  // Handle negative numbers correctly
+            remainderCount[remainder]++;
+        }
 
-        if(count % k == 0) return true;
-        else return false;
+        for (int i = 1; i < k; i++) {
+            if (remainderCount[i] != remainderCount[k - i]) {
+                return false;
+            }
+        }
+
+        return remainderCount[0] % 2 == 0;
     }
-}
+    }
