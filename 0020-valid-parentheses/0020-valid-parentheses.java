9ac5b847fbd3e3393a8af73953c4da63aeba4d87
Sivasubramani V <sivasubramani17v@gmail.com>
2024-10-10 09:44:44 +0530

    Time: 2 ms (52.91%), Space: 40.9 MB (98.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,40 @@
-class Solution {
-    public boolean isValid(String s) {
-      Deque<Character> stack = new ArrayDeque<>();
-      for( char c : s.toCharArray()){
-         if (c == '(')
-          stack.push(')');
-        else if (c == '{')
-          stack.push('}');
-        else if (c == '[')
-          stack.push(']');
-        else if (stack.isEmpty() || stack.pop() != c)
-          return false;
+// class Solution {
+//     public boolean isValid(String s) {
+//       Deque<Character> stack = new ArrayDeque<>();
+//       for( char c : s.toCharArray()){
+//          if (c == '(')
+//           stack.push(')');
+//         else if (c == '{')
+//           stack.push('}');
+//         else if (c == '[')
+//           stack.push(']');
+//         else if (stack.isEmpty() || stack.pop() != c)
+//           return false;
   
      
 
-    }
-     return stack.isEmpty();
-}
-}
+//     }
+//      return stack.isEmpty();
+// }
+// }
 
+class Solution{
+    public boolean isValid(String s){
+        HashMap<Character,Character> parenthesis = new HashMap<>();
+        parenthesis.put('(',')');
+        parenthesis.put('{','}');
+        parenthesis.put('[',']');
+        Stack<Character> stack = new Stack<>();
+        for( int i=0;i< s.length();i++){
+            if(parenthesis.containsKey(s.charAt(i))){
+                stack.push(s.charAt(i));
+            }
+            else{
+                if(stack.isEmpty() || parenthesis.get(stack.pop())!=s.charAt(i)){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
